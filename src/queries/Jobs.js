0457b24ea878232ecee3e686b4c4621b2d17c399
ankita shah ashwin <ankita.shah1301@gmail.com>
2020-06-01 21:24:05 +0200

    update queries

--- src/queries/Jobs.js
+++ src/queries/Jobs.js
@@ -1,13 +1,49 @@
 import { gql } from "apollo-boost";
 
 const QUERY_JOBS = gql`
-  query fetchJobs {
-    jobs {
+  query fetchJobs($limit: Int, $offset: Int) {
+    jobs(limit: $limit, offset: $offset) {
       id
       title
+      city
+      company_id
+      company {
+        id
+        name
+      }
+    }
+  }
+`;
+
+const FILTER_JOBS = gql`
+  query MyQuery(
+    $title: String
+    $company: Int
+    $city: String
+    $limit: Int
+    $offset: Int
+  ) {
+    jobs(
+      where: {
+        title: { _ilike: $title }
+        _or: {
+          city: { _eq: $city }
+          _or: { company: { id: { _eq: $company } } }
+        }
+      }
+      offset: $offset
+      limit: $limit
+    ) {
+      title
+      id
+      company_id
+      company {
+        name
+        id
+      }
       city
     }
   }
 `;
 
-export { QUERY_JOBS };
+export { QUERY_JOBS, FILTER_JOBS };
